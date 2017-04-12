0be69ac4288dd76df414cf0182cc0bc4a2353b67
masaka222 <masakaftw@hotmail.com>
2017-04-12 12:14:23 +0200

    Network-only activation of plugin #28
    Network-only activation of plugin #28

--- pressbooks-metadata/pressbooks-metadata.php
+++ pressbooks-metadata/pressbooks-metadata.php
@@ -23,6 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       pressbooks-metadata
  * Domain Path:       /languages
+ * Network: 		  True
  */
 
 // If this file is called directly, abort.
