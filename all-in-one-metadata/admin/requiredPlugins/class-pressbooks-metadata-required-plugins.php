9f8e1ad81ba5ccc15b1a505714f4f0bdb8c3276b
MashRoofa <christosv2@hotmail.com>
2017-08-29 17:21:37 +0200

    Added required plugin message in multisite admin page

--- all-in-one-metadata/admin/requiredPlugins/class-pressbooks-metadata-required-plugins.php
+++ all-in-one-metadata/admin/requiredPlugins/class-pressbooks-metadata-required-plugins.php
@@ -21,16 +21,17 @@ class Pressbooks_Metadata_Required_Plugins {
 	}
 
 	/**
-	 * A function that checks if third party required plugins are installed, if not it prompts the user to install them.
+	 * A function that checks if third party required plugins are installed, if not it prompts the user to install them (single site).
 	 * @since    0.x
 	 */
 	public static function install() {
 		// Check for custom-metadata install (Pressbooks includes custom-metadata plugin so we dont install it)
 		if(!site_cpt::pressbooks_identify()){
-					/*
-					 * Array of plugin arrays. Required keys are name and slug.
-					 * If the source is NOT from the .org repo, then source is also required.
-					 */
+
+			/*
+			 * Array of plugin arrays. Required keys are name and slug.
+			 * If the source is NOT from the .org repo, then source is also required.
+			 */
 			$plugins = array(
 				// This is an example of how to include a plugin bundled with a theme.
 				array(
@@ -46,6 +47,22 @@ class Pressbooks_Metadata_Required_Plugins {
 				)
 			);
 			tgmpa( $plugins );
+		}
+	}
+
+	/**
+	 * A function that checks if third party required plugins are installed, if not it prompts the user to download/install them (multisite site).
+	 * @since    0.x
+	 */
+	public static function check() {
+		if ( ! site_cpt::pressbooks_identify() ) {
+			//This is the error message for the network admin, it will pop up if the custom-metadata plugin is missing
+			if ( is_multisite() && ! is_plugin_active( 'custom-metadata/custom_metadata.php' ) ) {
+				$link =  plugin_dir_url('') . 'all-in-one-metadata\admin\requiredPlugins\plugins\custom-metadata.zip';
+				add_action( 'network_admin_notices', function () use ( $link ) {
+					echo '<div id="message" class="error fade"><p>Please make sure that the custom-metadata plugin is installed and activated for the full All In One Metadata Plugin Functionality -- <a href="' . $link . '">Get it Here</a></p></div>';
+				} );
+			}
 		}
 	}
 }
