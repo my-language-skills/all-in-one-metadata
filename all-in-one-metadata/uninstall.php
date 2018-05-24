d5caf1dcd80ae8e40ce05ef81428e1a23f5e5c7d
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-05-24 10:00:31 +0200

    uninstall delete options
    Since this commit after the plugin being uninstalled? all its related options will be deleted from the database

--- all-in-one-metadata/uninstall.php
+++ all-in-one-metadata/uninstall.php
@@ -29,3 +29,63 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+//get all the sites for multisite
+$blogs_ids=get_sites();
+
+//delete plugin options and posts/chapter related metadata from every site
+foreach( $blogs_ids as $b ){
+	switch_to_blog( $b->blog_id );
+
+	//get all the options from database
+	$all_options = wp_load_alloptions();
+	$plugin_options = [];
+	$related_meta = [];
+
+	//check if PressBooks plugin is used
+	if ((@include_once( WP_PLUGIN_DIR . '/pressbooks/pressbooks.php')) &&
+	    //Checking if the plugin is active
+	    is_plugin_active('pressbooks/pressbooks.php')) {
+		$pb = true;
+	}
+	else{
+		$pb = false;
+	}
+
+	//gather all post types, including built-in of without PressBooks
+	if($pb){
+		$allPostTypes = get_post_types( array( 'public' => true, '_builtin' => false )) ;
+	}else{
+		$allPostTypes = get_post_types( array( 'public' => true)) ;
+	}
+
+	$allPostTypes['site-meta'] = 'site-meta';
+	$allPostTypes['metadata'] = 'metadata';
+
+
+	//extract plugin options from all options
+	foreach ( $all_options as $name => $value ) {
+		foreach ($allPostTypes as $postType) {
+			if ( stristr( $name, '_type_' . $postType ) || stristr( $name, '_type_' . $postType . '_level' ) 
+				|| stristr( $name, '_type_overwrite' ) || stristr($name, 'saoverwr') || stristr($name, $postType.'_checkbox')) {
+				$plugin_options[ $name ] = $value;
+
+			}
+		}
+	}
+
+
+	//delete plugin options
+	foreach ( $plugin_options as $key => $value ) {
+		if ( get_option( $key ) || get_option($key, 'nonex') !== 'nonex') {
+			delete_option( $key );
+		}
+	}
+
+	// Delete Custom Post Type posts' meta
+
+	//$wpdb->query( "DELETE FROM {$wpdb->postmeta} meta WHERE `meta_key` LIKE '%\_type\_%' OR `meta_key` LIKE '%\_type\_%\_level'" );
+}
+
+
+
