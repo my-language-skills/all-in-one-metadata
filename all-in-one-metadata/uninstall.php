98f3a965271087c2804bc13eb4662c266bbca3b8
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-06-05 13:25:19 +0200

    uninstall fix

--- all-in-one-metadata/uninstall.php
+++ all-in-one-metadata/uninstall.php
@@ -76,10 +76,10 @@ foreach( $blogs_ids as $b ){
 	//extract plugin options from all options
 	foreach ( $all_options as $name => $value ) {
 		foreach ($allPostTypes as $postType) {
-			if ( stristr( $name, '_type_' . $postType ) || stristr( $name, '_type_' . $postType . '_level' ) 
+			if ( stristr( $name, '_type_' . $postType ) || stristr( $name, '_types_' . $postType . '_level' )
 				|| stristr( $name, '_type_overwrite' ) || stristr($name, 'saoverwr') || stristr($name, $postType.'_checkbox')
 				|| stristr( $name, 'dublin_checkbox' ) || stristr($name, 'coins_checkbox') || stristr($name, 'educational_checkbox')
-			    || $name == 'property_network_value' || $name == 'property_network_value_freeze') {
+			    || $name == 'property_network_value' || $name == 'property_network_value_freeze' || $name == 'schema_locations') {
 				$plugin_options[ $name ] = $value;
 
 			}
